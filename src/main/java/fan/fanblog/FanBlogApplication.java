4575a89f322ec4d73ffa70091543882e45da43ea
Fan223 <fanj.zh@foxmail.com>
2022-07-26 01:28:28 +0800

    创建项目

---
+++ src/main/java/fan/fanblog/FanBlogApplication.java
@@ -0,0 +1,13 @@
+package fan.fanblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanBlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/fan/fanblog/entity/BlogDO.java
@@ -0,0 +1,7 @@
+package fan.fanblog.entity;
+
+import java.io.Serializable;
+
+public class BlogDO implements Serializable {
+
+}
